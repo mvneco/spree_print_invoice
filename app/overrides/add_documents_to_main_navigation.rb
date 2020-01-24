f2464906406dc898c805addf2d740ce71727c9c2
igor-djuric-sedecom <59830974+igor-djuric-sedecom@users.noreply.github.com>
2020-01-24 14:25:28 +0100

    Disabled as original HTML content is garbled and selectors placed here won't work

--- app/overrides/add_documents_to_main_navigation.rb
+++ app/overrides/add_documents_to_main_navigation.rb
@@ -1,4 +1,5 @@
 Deface::Override.new(
+  disabled:      true,
   virtual_path:  'spree/layouts/admin',
   insert_bottom: '[data-hook="admin_tabs"] > nav',
   partial:       'spree/admin/shared/menu/documents_tab',
