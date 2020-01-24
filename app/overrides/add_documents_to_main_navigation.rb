07363b3e327fb381e2b008f1f1c42224a9a6ac42
igor-djuric-sedecom <59830974+igor-djuric-sedecom@users.noreply.github.com>
2020-01-24 13:10:13 +0100

    Another Deface selector fix

--- app/overrides/add_documents_to_main_navigation.rb
+++ app/overrides/add_documents_to_main_navigation.rb
@@ -1,6 +1,6 @@
 Deface::Override.new(
   virtual_path:  'spree/layouts/admin',
-  insert_before:  "erb[loud]:contains('</nav>')",
+  insert_bottom: 'nav',
   partial:       'spree/admin/shared/menu/documents_tab',
   name:          'documents_tab'
 )
