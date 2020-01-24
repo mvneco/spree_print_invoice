9f4f904d0028fb7a91a6bc56dae6a62888a8215e
igor-djuric-sedecom <59830974+igor-djuric-sedecom@users.noreply.github.com>
2020-01-24 13:03:02 +0100

    Fix for the code adaptation to Spree 4.0.3

--- app/overrides/add_documents_to_main_navigation.rb
+++ app/overrides/add_documents_to_main_navigation.rb
@@ -1,6 +1,6 @@
 Deface::Override.new(
   virtual_path:  'spree/layouts/admin',
-  insert_after:  'ul.nav-sidebar',
+  insert_before:  "erb[loud]:contains('</nav>')",
   partial:       'spree/admin/shared/menu/documents_tab',
   name:          'documents_tab'
 )
