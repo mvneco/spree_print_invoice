0069e013ca8e87d93ad0dcdf619d51000f3a0992
igor-djuric-sedecom <59830974+igor-djuric-sedecom@users.noreply.github.com>
2020-01-24 12:10:08 +0100

    Adapted to Spree 4.0.3 codebase

--- app/overrides/add_documents_to_main_navigation.rb
+++ app/overrides/add_documents_to_main_navigation.rb
@@ -1,6 +1,6 @@
 Deface::Override.new(
   virtual_path:  'spree/layouts/admin',
-  insert_bottom: 'ul.nav-sidebar',
+  insert_after:  'ul.nav-sidebar',
   partial:       'spree/admin/shared/menu/documents_tab',
   name:          'documents_tab'
 )
