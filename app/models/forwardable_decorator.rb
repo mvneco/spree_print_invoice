e5e54f7ce281012570c34ee5d4415f9386c72370
igor-djuric-sedecom <59830974+igor-djuric-sedecom@users.noreply.github.com>
2020-01-28 14:26:47 +0100

    Replace self with base

--- app/models/forwardable_decorator.rb
+++ app/models/forwardable_decorator.rb
@@ -9,8 +9,8 @@ module ForwardableDecorator
     return if self.class_variable_get(:@@already_prepended)
     self.class_variable_set(:@@already_prepended, true)
 
-    self.remove_method :delegate
-    self.remove_method :instance_delegate
+    base.remove_method :delegate
+    base.remove_method :instance_delegate
   end
 
 end
