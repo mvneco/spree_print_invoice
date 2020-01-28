7459ff9a9212b081d4dac600d0df648cda7a583c
igor-djuric-sedecom <59830974+igor-djuric-sedecom@users.noreply.github.com>
2020-01-28 14:19:42 +0100

    Zeitwerk:check error fix

---
+++ app/models/forwardable_decorator.rb
@@ -0,0 +1,17 @@
+module ForwardableDecorator
+
+  # Crucial to prevent multiple invocations of self.prepended(base)!
+  @@already_prepended = false unless defined? @@already_prepended
+
+  def self.prepended(base)
+
+    # Crucial to prevent multiple invocations of self.prepended(base)!
+    return if self.class_variable_get(:@@already_prepended)
+    self.class_variable_set(:@@already_prepended, true)
+
+    self.remove_method :delegate
+  end
+
+end
+
+Forwardable.prepend(ForwardableDecorator)
