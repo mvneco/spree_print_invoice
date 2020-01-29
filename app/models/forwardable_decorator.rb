5857a211fae3a0cef57b29873007e6055566d12d
igor-djuric-sedecom <59830974+igor-djuric-sedecom@users.noreply.github.com>
2020-01-29 11:56:19 +0100

    Syntax error fix

--- app/models/forwardable_decorator.rb
+++ app/models/forwardable_decorator.rb
@@ -9,8 +9,8 @@ module ForwardableDecorator
     return if self.class_variable_get(:@@already_prepended)
     self.class_variable_set(:@@already_prepended, true)
 
-    base.remove_method :delegate if !base.nil? && base.respond_to? :delegate
-    base.remove_method :instance_delegate if !base.nil? && base.respond_to? :instance_delegate
+    base.remove_method :delegate if (!base.nil?) && (base.respond_to? :delegate)
+    base.remove_method :instance_delegate if (!base.nil?) && (base.respond_to? :instance_delegate)
   end
 
 end
